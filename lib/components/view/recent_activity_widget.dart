d30b00e2022a22848c3b2f5fd51fbdb67a4df53d
bath0ry <plylegacy@gmail.com>
2023-01-23 14:52:35 -0300

    add ink well and set state recent activity

--- lib/components/view/recent_activity_widget.dart
+++ lib/components/view/recent_activity_widget.dart
@@ -5,7 +5,6 @@ import 'package:alubank_flutter/theme/theme_colors.dart';
 
 import 'package:flutter/material.dart';
 
-
 class RecentActivity extends StatelessWidget {
   const RecentActivity({super.key});
 
@@ -18,89 +17,101 @@ class RecentActivity extends StatelessWidget {
   }
 }
 
-class _RecentActivityContent extends StatelessWidget {
+class _RecentActivityContent extends StatefulWidget {
   const _RecentActivityContent({super.key});
 
   @override
+  State<_RecentActivityContent> createState() => _RecentActivityContentState();
+}
+
+class _RecentActivityContentState extends State<_RecentActivityContent> {
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: DotWidget(color: ThemeColors.recentActivity['spent']),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Saída',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    Text(
-                      '\$10.000,00',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    )
-                  ],
-                )
-              ],
-            ),
-            Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(right: 8),
-                  child: DotWidget(color: ThemeColors.recentActivity['income']),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      'Entrada',
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                    Text(
-                      '\$ 9.997,00',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    )
-                  ],
-                )
-              ],
-            ),
-          ],
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 16, bottom: 8),
-          child: Text(
-            'Limite de gastos: \$500,00',
+    return InkWell(
+      onTap: () {
+        setState(() {});
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child:
+                        DotWidget(color: ThemeColors.recentActivity['spent']),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Saída',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        '\$10.000,00',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child:
+                        DotWidget(color: ThemeColors.recentActivity['income']),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Entrada',
+                        style: Theme.of(context).textTheme.bodyMedium,
+                      ),
+                      Text(
+                        '\$ 9.997,00',
+                        style: Theme.of(context).textTheme.bodyLarge,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ],
           ),
-        ),
-        Container(
-          clipBehavior: Clip.hardEdge,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(20))),
-          child: const LinearProgressIndicator(
-            value: 0.5,
-            minHeight: 7,
+          const Padding(
+            padding: EdgeInsets.only(top: 16, bottom: 8),
+            child: Text(
+              'Limite de gastos: \$500,00',
+            ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 12, bottom: 12),
-          child: DivisionWidget(),
-        ),
-        const Text(
-            'Esse mês você gastou \$1500.00 com jogos. Tente abaixar esse custo!'),
-        TextButton(
-            onPressed: () {},
-            child: const Text(
-              'Diga-me como!',
-              style: TextStyle(fontSize: 17),
-            ))
-      ],
+          Container(
+            clipBehavior: Clip.hardEdge,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            child: const LinearProgressIndicator(
+              value: 0.5,
+              minHeight: 7,
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 12, bottom: 12),
+            child: DivisionWidget(),
+          ),
+          const Text(
+              'Esse mês você gastou \$1500.00 com jogos. Tente abaixar esse custo!'),
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Diga-me como!',
+                style: TextStyle(fontSize: 17),
+              ))
+        ],
+      ),
     );
   }
 }
