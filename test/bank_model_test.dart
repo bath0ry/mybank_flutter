82952d894cea047e9435309691864f1dcc5ef520
bath0ry <plylegacy@gmail.com>
2023-01-25 15:33:07 -0300

    add group test for deposit and transfer/create test transfer

--- test/bank_model_test.dart
+++ test/bank_model_test.dart
@@ -2,9 +2,18 @@ import 'package:alubank_flutter/data/bank_model.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Bank model deposit should turns into 10', () {
-    final bank = BankModel();
-    bank.deposit(10);
-    expect(bank.points, (10));
+  group("Bank model deposit tests ", () {
+    test('Bank model deposit should turns into 10', () {
+      final bank = BankModel();
+      bank.deposit(10);
+      expect(bank.points, (10));
+    });
+  });
+  group('Bank model transfer tests', () {
+    test('Bank model transfer should turns into 10', () {
+      final bank = BankModel();
+      bank.transfer(10);
+      expect(bank.points, (10));
+    });
   });
 }
