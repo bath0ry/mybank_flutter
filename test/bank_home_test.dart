07bad3e8e0156132b246e1bb78a0c1deded0f9b9
bath0ry <plylegacy@gmail.com>
2023-01-30 20:15:23 -0300

    add actions tests/widgets tests/organize teste in group

--- test/bank_home_test.dart
+++ test/bank_home_test.dart
@@ -5,30 +5,48 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('My widget has "Spent"', (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: BankInherited(child: HomePage()),
-    ));
-    final spentFinder = find.text('Saída');
-    expect(spentFinder, findsOneWidget);
-  });
-  testWidgets('My widget has LinearProgressIndicator', (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: BankInherited(child: HomePage()),
-    ));
+  //teste de widgets
 
-    expect(find.byType(LinearProgressIndicator), findsOneWidget);
+  group('Widget tests', () {
+    testWidgets('My widget has "Spent"', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: BankInherited(child: const HomePage()),
+      ));
+      final spentFinder = find.text('Saída');
+      expect(spentFinder, findsOneWidget);
+    });
+    testWidgets('My widget has LinearProgressIndicator', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: BankInherited(child: const HomePage()),
+      ));
+
+      expect(find.byType(LinearProgressIndicator), findsOneWidget);
+    });
+    testWidgets('Finds 5 box card', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: BankInherited(child: const HomePage()),
+      ));
+      expect(find.byWidgetPredicate((widget) {
+        if (widget is BoxHome) {
+          return true;
+        } else {
+          return false;
+        }
+      }), findsNWidgets(5));
+    });
   });
-  testWidgets('Finds 5 box card', (tester) async {
-    await tester.pumpWidget(MaterialApp(
-      home: BankInherited(child: HomePage()),
-    ));
-    expect(find.byWidgetPredicate((widget) {
-      if (widget is BoxHome) {
-        return true;
-      } else {
-        return false;
-      }
-    }), findsNWidgets(5));
+
+  //teste de ações
+
+  group('Actions tests', () {
+    testWidgets('Earned value should turns into 10', (tester) async {
+      await tester.pumpWidget(MaterialApp(
+        home: BankInherited(child: HomePage()),
+      ));
+      await tester.tap(find.text('Depositar'));
+      await tester.tap(find.text('Entrada'));
+      await tester.pumpAndSettle();
+      expect(find.text('\$10.0'), findsOneWidget);
+    });
   });
 }
